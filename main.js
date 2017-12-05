053c5668c052d6ec67a309c7be15efff854bf015
Pavel88py <silber.ypm@gmail.com>
2017-12-05 11:24:02 +0500

    First version - carousel with draggable items

---
+++ main.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+
+
+    var $btnLeft = $('#btnLeft');
+    var $btnRight = $('#btnRight');
+    var $slides = $('.img');
+    // var ACTIVE_ITEM = 'active';
+
+    // $slides.not('.' + ACTIVE_ITEM).hide(); //все .img кроме ACTIVE_ITEM
+
+    $('.group1').show();
+    $('.group2').hide();
+
+    $btnLeft.click(function() {
+        if ($slides.hasClass('group1 active')) {
+            $('.group1').hide('slow').removeClass('active');
+            $('.group2').show('slow').addClass('active');
+        } else {
+            $('.group2').hide('slow').removeClass('active');
+            $('.group1').show('slow').addClass('active');
+        }
+    });
+
+    $btnRight.click(function() {
+        if ($slides.hasClass('group1 active')) {
+            $('.group1').hide('slow').removeClass('active');
+            $('.group2').show('slow').addClass('active');
+        } else {
+            $('.group2').hide('slow').removeClass('active');
+            $('.group1').show('slow').addClass('active');
+        }
+    });
+
+
+
+
+    $slides.mouseover(function() {
+        $(this).attr('id', 'draggable');
+        $(this).addClass('draggable ui-draggable ui-draggable-handle');
+        // $(this).addClass('ui-draggable ui-draggable-handle');
+        $(function() {
+            $("#draggable").draggable({
+                revert: true //Элемент уходит на свою позицию
+            })
+        });
+
+    });
+
+    $slides.mouseleave(function() {
+        $(this).removeAttr('id');
+        $(this).removeClass('draggable ui-draggable ui-draggable-handle');
+        // $(this).removeClass('ui-draggable ui-draggable-handle');
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
