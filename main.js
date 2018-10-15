f3f59035279b71d1a5b3ab2ff16a4937c7111898
Pavel Silber <34262772+silberpavel@users.noreply.github.com>
2018-10-15 14:47:36 +0300

    images

--- main.js
+++ main.js
@@ -4,13 +4,11 @@ $(document).ready(function() {
     var $btnLeft = $('#btnLeft');
     var $btnRight = $('#btnRight');
     var $slides = $('.img');
-    // var ACTIVE_ITEM = 'active';
-
-    // $slides.not('.' + ACTIVE_ITEM).hide(); //все .img кроме ACTIVE_ITEM
 
     $('.group1').show();
     $('.group2').hide();
 
+    // Buttons 
     $btnLeft.click(function() {
         if ($slides.hasClass('group1 active')) {
             $('.group1').hide('slow').removeClass('active');
@@ -31,34 +29,32 @@ $(document).ready(function() {
         }
     });
 
-
-
-
+    // DRAG AND DROP
     $slides.mouseover(function() {
         $(this).attr('id', 'draggable');
         $(this).addClass('draggable ui-draggable ui-draggable-handle');
-        // $(this).addClass('ui-draggable ui-draggable-handle');
         $(function() {
             $("#draggable").draggable({
                 revert: true //Элемент уходит на свою позицию
             });
+
             $("#droppable").droppable({
                 drop: function(event, ui) {
                     $(this)
                         .addClass("ui-state-highlight")
                         .find("p")
                         .html("Dropped!" + ($('#draggable').attr('alt')));
-                    // console.log();
                 }
             });
         });
 
     });
 
+    // drag leave to the basket and get item info 
+
     $slides.mouseleave(function() {
         $(this).removeAttr('id');
         $(this).removeClass('draggable ui-draggable ui-draggable-handle');
-        // $(this).removeClass('ui-draggable ui-draggable-handle');
     });
 
 
